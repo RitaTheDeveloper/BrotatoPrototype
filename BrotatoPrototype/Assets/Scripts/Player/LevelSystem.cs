de4988e899bd237d67d4771b6d1448abcb874dab
PhysicalChief <alexsoldat98@gmail.com>
2024-03-13 22:43:32 +0300

    Обновил свою сцену, фикс двойного звука подбора монетки

--- BrotatoPrototype/Assets/Scripts/Player/LevelSystem.cs
+++ BrotatoPrototype/Assets/Scripts/Player/LevelSystem.cs
@@ -51,7 +51,6 @@ public class LevelSystem : MonoBehaviour
             if (hitColliders[i].gameObject.tag == "Currency")
             {
                 //audioSource.PlayOneShot(AudioManager.instance.GetAudioClip("Coin"));
-                AudioManager.instance.Play("Coin");
                 Transform currency = hitColliders[i].gameObject.transform;
                 Vector3 targetPosition = new Vector3(transform.position.x, currency.transform.position.y, transform.position.z);
                 currency.position = Vector3.MoveTowards(currency.position, targetPosition, 40f * Time.deltaTime);
@@ -62,6 +61,7 @@ public class LevelSystem : MonoBehaviour
                     currencyController.ChangeTotalAmountOfCurrency(1);
                     IncreaseCurrentExperience(xp);
                     currency.GetComponent<Currency>().PutAwayFromScene();
+                    AudioManager.instance.Play("Coin");
                 }
             }
         }
