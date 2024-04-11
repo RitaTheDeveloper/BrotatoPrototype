7c84179a6594199ccfe9409fdb60e663fb276d98
RitaTheDeveloper <ritagrashchenkova@gmail.com>
2024-04-12 00:48:41 +0300

    новые анимации для ворона двух тиров и для волков

--- BrotatoPrototype/Assets/Scripts/Enemy/EnemyCowardController.cs
+++ BrotatoPrototype/Assets/Scripts/Enemy/EnemyCowardController.cs
@@ -8,24 +8,6 @@ public class EnemyCowardController : EnemyController
     [SerializeField] private float _escapeSpeed;
     private float distanceDelay = 1f;
 
-    private float _timer = 0f;
-    private void FixedUpdate()
-    {
-        //float distance = Vector3.Distance(transform.position, target.position);
-
-        //if (distance >= _distanceFromPlayer + distanceDelay)
-        //{
-        //    Chase();
-        //}
-        ////else if (distance >=_distanceFromPlayer && distance < _distanceFromPlayer + distanceDelay)
-        ////{
-        ////    navMeshAgent.speed = 0f;
-        ////}
-        //else if (distance < _distanceFromPlayer)
-        //{
-        //    Run();
-        //}       
-    }
     private void Update()
     {
         float distance = Vector3.Distance(transform.position, target.position);
@@ -59,19 +41,7 @@ public class EnemyCowardController : EnemyController
         {
             navMeshAgent.speed = _escapeSpeed;
             navMeshAgent.acceleration = 300f;
-            navMeshAgent.SetDestination(newPos);
-
-            if (!navMeshAgent.pathPending)
-            {
-                if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
-                {
-                    if (!navMeshAgent.hasPath || navMeshAgent.velocity.sqrMagnitude < 0.3f)
-                    {
-                        Debug.Log("обнуляем");
-                        _timer = 0f;
-                    }
-                }
-            }
+            navMeshAgent.SetDestination(newPos);            
         }
                 
     }
