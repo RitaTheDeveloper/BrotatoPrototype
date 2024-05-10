cd2d20530dc6f34413e53ceb73a68865b1b51a9c
PhysicalChief <alexsoldat98@gmail.com>
2024-05-10 18:58:12 +0300

    фикс списка предметов в магазине

--- BrotatoPrototype/Assets/Scripts/Managers/UIShop.cs
+++ BrotatoPrototype/Assets/Scripts/Managers/UIShop.cs
@@ -175,7 +175,7 @@ public class UIShop : MonoBehaviour
 
     public void CreateItemsElements(List<StandartItem> _items)
     {
-        if (items.Count > listSlotsOfItems.Count)
+        if (_items.Count > listSlotsOfItems.Count)
         {
             CreateSlotsForItems();
         }
