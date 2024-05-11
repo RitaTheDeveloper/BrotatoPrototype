a21c6b858326008ccb615de13bcc6f30afc3f0cf
RitaTheDeveloper <ritagrashchenkova@gmail.com>
2024-05-11 19:50:06 +0300

    баба яга

--- BrotatoPrototype/Assets/Scripts/Managers/GameManager.cs
+++ BrotatoPrototype/Assets/Scripts/Managers/GameManager.cs
@@ -32,7 +32,7 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         instance = this;
-       // ResetProgress();
+        ResetProgress();
     }
     private void Start()
     {
