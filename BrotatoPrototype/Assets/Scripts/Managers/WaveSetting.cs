d4c3570e80133afe9c6252a6fbd15a5c292ed8a4
RitaTheDeveloper <ritagrashchenkova@gmail.com>
2024-07-12 13:29:22 +0300

    fixed error WaveSettings

--- BrotatoPrototype/Assets/Scripts/Managers/WaveSetting.cs
+++ BrotatoPrototype/Assets/Scripts/Managers/WaveSetting.cs
@@ -6,9 +6,6 @@ using UnityEngine;
 public class WaveSetting
 {
     public string name;
-    //public float _waveTime = 30f;
-    //public int _amountOfGoldPerWave = 100;
-    //public int _amountOfExpPerWave = 100;
     public ParametersOfWave parametersOfWave = new ParametersOfWave(30, 100, 100);
     public EnemyStrengthFactors _enemyStrengthFactors = new EnemyStrengthFactors(1f, 1f, 1f);
     public List<EnemySpawnerSettings> enemySpawnerSettings = new List<EnemySpawnerSettings>();
@@ -19,17 +16,3 @@ public class WaveSetting
     public WaveController Wave { get => wave; set => wave = value; }    
 }
 
-//[System.Serializable]
-//public class ParametersOfWave
-//{
-//    public float waveTime = 30f;
-//    public int amountOfGoldPerWave = 100;
-//    public int amountOfExpPerWave = 100;
-
-//    public ParametersOfWave(float time, int amountOfGold, int amountOfExp)
-//    {
-//        waveTime = time;
-//        amountOfGoldPerWave = amountOfGold;
-//        amountOfExpPerWave = amountOfExp;
-//    }
-//}
