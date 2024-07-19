bef63179181c090f9fb33450c9a2e9454b3c9577
NasysMeiner <iguana10053@gmail.com>
2024-07-19 04:30:27 +0300

    update

--- BrotatoPrototype/Assets/Scripts/UI/Tooltip/TooltipComponent.cs
+++ BrotatoPrototype/Assets/Scripts/UI/Tooltip/TooltipComponent.cs
@@ -7,36 +7,71 @@ public class TooltipComponent : MonoBehaviour, IPointerEnterHandler, IPointerExi
     [SerializeField] private Tooltip _prefabTooltip;
     [SerializeField] private string _tooltipText;
     [SerializeField] private Sprite _sprite = null;
-    [SerializeField] private float _timeShow = 1f;
 
     private Tooltip _tooltip;
+    private Vector2 _sizeCanvas;
+    private RectTransform _rectTransform;
 
+    private bool _isWork = false;
     private Vector3 _mousePosition;
 
     private void Awake()
     {
         _tooltip = Instantiate(_prefabTooltip, transform);
         _tooltip.InitTooltip(_tooltipText, _sprite);
-        _tooltip.gameObject.SetActive(false);
+        _tooltip.Disable();
+
+        GameObject obj = transform.parent.gameObject;
+
+        while (true)
+        {
+            if (obj != null && obj.transform.parent != null)
+            {
+                if (obj.TryGetComponent(out Canvas canvas))
+                {
+                    _rectTransform = canvas.gameObject.GetComponent<RectTransform>();
+                    _sizeCanvas = _rectTransform.sizeDelta * _rectTransform.localScale;
+
+                    break;
+                }
+                else
+                {
+                    obj = obj.transform.parent.gameObject;
+                }
+            }
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        StartCoroutine(ShowTooltip(_timeShow));
+        _isWork = true;
+        _tooltip.Enable();
+        StartCoroutine(ShowTooltip());
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        _tooltip.gameObject.SetActive(false);
+        _isWork = false;
+        _tooltip.Disable();
     }
 
-    private IEnumerator ShowTooltip(float time)
+    private IEnumerator ShowTooltip()
     {
-        yield return new WaitForSeconds(time);
+        while (_isWork)
+        {
+            _mousePosition = Input.mousePosition;
+            _sizeCanvas = _rectTransform.sizeDelta * _rectTransform.localScale;
 
-        _mousePosition = Input.mousePosition;
+            CheckInWindow();
+            _tooltip.gameObject.transform.position = _mousePosition;
 
-        _tooltip.transform.position = _mousePosition;
-        _tooltip.gameObject.SetActive(true);
+            yield return null;
+        }
+    }
+
+    private void CheckInWindow()
+    {
+        Vector2 newPos = new Vector2(_mousePosition.x + _tooltip.GetSize.x * _rectTransform.localScale.x < _sizeCanvas.x ? 0 : -_tooltip.GetSize.x, _mousePosition.y + _tooltip.GetSize.y * _rectTransform.localScale.y < _sizeCanvas.y ? 0 : -_tooltip.GetSize.y);
+        _tooltip.SetPosition(newPos);
     }
 }
