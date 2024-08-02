a22100e61fad9725ea99903385f4606736e944fc
Blavez <76618921+Blavez@users.noreply.github.com>
2024-08-02 19:41:27 +0300

    Первичная настройка

--- BrotatoPrototype/Assets/Editor/ReadOnlyInspectorDrawer.cs
+++ BrotatoPrototype/Assets/Editor/ReadOnlyInspectorDrawer.cs
@@ -1,5 +1,6 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEditorInternal;
 
 [CustomPropertyDrawer(typeof(ReadOnlyInspectorAttribute))]
 public class ReadOnlyInspectorDrawer : PropertyDrawer
